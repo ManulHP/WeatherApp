2da01ce3926290c6a7d22cbf7b667b9a1d6bc0fa
ManulHP <manulhpallege@gmail.com>
2022-06-02 19:52:16 +0530

    Fix text validation alert

--- WeatherApp/WeatherApp/screen/SearchScreen.swift
+++ WeatherApp/WeatherApp/screen/SearchScreen.swift
@@ -14,6 +14,7 @@ struct SearchScreen: View {
     @StateObject var controller = WeatherController()
 
     @State private var weatherUnits: Units = .metric
+    @State private var isAlertShowing: Bool = false
     
     var body: some View {
         ZStack {
@@ -26,11 +27,24 @@ struct SearchScreen: View {
                     Button {
                         // action
                         Task {
-                            await controller.searchCity(cityName: self.textController, weatherUnit: self.weatherUnits)
+                            let charset = CharacterSet(charactersIn:"1234567890.!@#$%^&*()<>/?{}[]|=+_~`")
+                            if(textController.rangeOfCharacter(from: charset) != nil) {
+                                isAlertShowing.toggle()
+                            }else {
+                                await controller.searchCity(cityName: self.textController, weatherUnit: self.weatherUnits)
+                            }
+                            
                         }
                     } label: {
                         Image(systemName: "magnifyingglass")
                             .padding()
+                    }
+                    .alert(isPresented: $isAlertShowing) {
+                        Alert(
+                            title: Text("Warning!"),
+                            message: Text("Please check the city name."),
+                            dismissButton: .default(Text("Got it!"))
+                        )
                     }
                 }
                 
@@ -60,7 +74,7 @@ struct SearchScreen: View {
                                 Text("\(item.value)%")
                                     .font(.system(size: 20, weight: .regular, design: .rounded))
                             }else if (item.title == "Pressure"){
-                                Text("\(item.value)")
+                                Text("\(item.value) Pa")
                                     .font(.system(size: 20, weight: .regular, design: .rounded))
                             }else if (item.title == "Wind Speed"){
                                 Text(self.weatherUnits == .metric ? "\(item.value)km/h" : "\(item.value)m/h")
